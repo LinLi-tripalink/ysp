1b9bbe1c861e2aa70efd02f3e2f921ff81a79fcf
Lin Li <lin.li@chn.tripalink.com>
2023-11-06 15:17:10 +0800

    feat: update config

--- dspIM/config.js
+++ dspIM/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-	'REDISHOST' : '127.0.0.1',
-	'REDISPASS' : 'dfr38ck.',
-	'REDISPORT' : '6379',
-	'TOKEN'		: '1234567',
-	'sign_key'	: '562d4226cb2a2b4f74b3ef4340828b5d',
+	'REDISHOST': '127.0.0.1',
+	'REDISPASS': 'dfr38ck.',
+	'REDISPORT': '6379',
+	'TOKEN': '1234567',
+	'sign_key': '562d4226cb2a2b4f74b3ef4340828b5d',
 	'socket_port': '19967',
-	'WEBADDRESS': 'http://live.xinghuijz.cn/appapi/public/' //站点域名
+	'WEBADDRESS': 'http://3shaai.cn/appapi/public/' //站点域名
 }
